99e3d29747432fe50d6b56ad85933a87f69c1085
Reid Sherman <reidsherman@gmail.com>
2020-10-28 12:15:37 -0700

    Updates

---
+++ precache-manifest.86976b59bbcba078020f295d245c902d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43e54a4091a9bca19995e4614ac443bf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "06d2b86505afaf5af03e",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "9cc169620062ef59cc0e",
+    "url": "./static/js/2.aec88412.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "./static/js/2.aec88412.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06d2b86505afaf5af03e",
+    "url": "./static/js/main.87011497.chunk.js"
+  },
+  {
+    "revision": "de8d761f20b09ef97703",
+    "url": "./static/js/runtime-main.88dd6863.js"
+  }
+]);
